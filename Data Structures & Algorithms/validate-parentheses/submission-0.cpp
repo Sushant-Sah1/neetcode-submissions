57c8f4724f26b8ac3ea1b77b6144b9c127e2412e
SinArchbishop_of_Gluttony <sushant210706@gmail.com>
2026-06-02 10:20:14 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto i:s){
+            if(st.empty()){
+                st.push(i);
+            }else{
+                if(i==')' && st.top()=='('){
+                    st.pop();
+                }else if(i==']' && st.top()=='['){
+                    st.pop();
+                }else if(i=='}' && st.top()=='{'){
+                    st.pop();
+                }else{
+                    st.push(i);
+                }
+            }
+        }
+        return st.size()==0;
+    }
+};
