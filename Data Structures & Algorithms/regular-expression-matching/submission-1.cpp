9f06e6cb57cfeaee02755505f33bdc4c8e3a9490
SinArchbishop_of_Gluttony <sushant210706@gmail.com>
2026-06-25 11:04:04 +0530

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.size(),m=p.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        int t=0;
+        for(int i=1;i<=m;i++){
+            if(p[i-1]!='*'){
+                t++;
+            }else{
+                t--;
+            }
+            if(t==0){
+                dp[0][i]=true;
+            }
+        }
+        dp[0][0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(p[j-1]=='.'){
+                    dp[i][j]=dp[i][j] || dp[i-1][j-1];
+                }else if(p[j-1]=='*'){
+                    // not matching with previous at all
+                    dp[i][j]=dp[i][j] || dp[i][j-2];
+
+                    // matching once
+
+                    if(s[i-1]==p[j-2] || p[j-2]=='.'){
+                        dp[i][j]=dp[i][j] || dp[i-1][j-1];
+                    }
+
+                    // matching mutliple times
+
+                    if(s[i-1]==p[j-2] || p[j-2]=='.'){
+                        dp[i][j]=dp[i][j] || dp[i-1][j];
+                    }
+
+
+                }else if(s[i-1]==p[j-1]){
+                    dp[i][j]=dp[i][j] || dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
